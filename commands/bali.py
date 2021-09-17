78f56db0f50eb05402d36d2f90757bae5ab91405
Oliver Lin <jumpy12359@gmail.com>
2021-09-17 22:11:54 +1000

    Formatting

--- commands/bali.py
+++ commands/bali.py
@@ -7,12 +7,10 @@ component = tanjun.Component()
 @component.with_command
 @tanjun.as_message_command("bali", "八狸")
 async def command_bali(ctx: tanjun.abc.Context) -> None:
-	responses = ['八狸可愛', '八狸可幹', '八狸可口']
-	await ctx.message.delete()
-	await ctx.respond(random.choice(responses))
+    responses = ['八狸可愛', '八狸可幹', '八狸可口']
+    await ctx.message.delete()
+    await ctx.respond(random.choice(responses))
 
 @tanjun.as_loader
 def load_component(client: tanjun.abc.Client) -> None:
-    # This loads the component, and is necessary in EVERY module,
-    # otherwise you'll get an error.
     client.add_component(component.copy())
