5f069542904f6488d266349dd303d4b5b141fa66
Oliver Lin <jumpy12359@gmail.com>
2021-09-17 19:19:22 +1000

    Add extra responses for 'bali' command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
 # Changelog
 All updates and changes made to this bot will be documented here.
 
+## [0.0.3] - 17/09/2021
+### Changed
+- Added extra responses for 'bali' command
+
 ## [0.0.2] - 17/09/2021
 ### Added
-- Dice command
-- Bali command
+- 'dice' command
+- 'bali' command
 
 ## [0.0.1] - 17/09/2021
 ### Added
--- commands/bali.py
+++ commands/bali.py
@@ -1,3 +1,5 @@
+import random
+
 import tanjun
 
 component = tanjun.Component()
@@ -5,8 +7,9 @@ component = tanjun.Component()
 @component.with_command
 @tanjun.as_message_command("bali", "八狸")
 async def command_bali(ctx: tanjun.abc.Context) -> None:
+	responses = ['八狸可愛', '八狸可幹', '八狸可口']
 	await ctx.message.delete()
-	await ctx.respond("八狸可愛")
+	await ctx.respond(random.choice(responses))
 
 @tanjun.as_loader
 def load_component(client: tanjun.abc.Client) -> None:
